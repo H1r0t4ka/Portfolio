4a07ce3d510162b7969b79704b4ab28bbe9667ce
H1r0t4ka <juanito_pachanga@hotmail.com>
2025-10-13 16:54:19 -0500

    Se implementa SEO básico y se corrigen algunos aspectos de rendimiento.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://juanmanuelquevedo.dev/', // ¡Reemplaza con tu dominio real!
   vite: {
     plugins: [tailwindcss()]
   },
